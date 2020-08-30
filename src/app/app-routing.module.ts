efd8766ac2fc99e1c875b88ed3b9398c039b70c9
Tony Cao <tony.cao1@outlook.com>
2020-08-30 16:58:21 -0400

    Generated home, work and projects page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/pages/home/home.component';
+import { ProjectsComponent } from '../app/pages/projects/projects.component';
+import { WorkComponent } from '../app/pages/work/work.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'work', component: WorkComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WorkComponent } from './pages/work/work.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorkComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
