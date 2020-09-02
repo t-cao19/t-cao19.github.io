e68d374023487823ace259447b7ab5f80bb5cf20
Tony Cao <tony.cao1@outlook.com>
2020-09-02 18:28:56 -0400

    Rerouted navigation bar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  home(): void {
+    this.router.navigate(['/home']).then(() => {
+      setTimeout(function () {
+        window.location.reload();
+      }, 100);
+    });
   }
 
+  resume(): void {
+    this.router.navigate(['/resume']).then(() => {
+      setTimeout(function () {
+        window.location.reload();
+      }, 100);
+    });
+  }
+
+  projects(): void {
+    this.router.navigate(['/projects']).then(() => {
+      setTimeout(function () {
+        window.location.reload();
+      }, 100);
+    });
+  }
+
+  volunteer(): void {
+    this.router.navigate(['/volunteer']).then(() => {
+      setTimeout(function () {
+        window.location.reload();
+      }, 100);
+    });
+  }
 }
