bdc4e5252cc60f74d2de0cb425444b7ffb85e0b2
Tony Cao <tony.cao1@outlook.com>
2020-09-02 22:32:00 -0400

    Allowed for local JSON file imports

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import courses from '../../../assets/data/courses.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  courses: any[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.courses = courses;
   }
 
+  ngOnInit(): void {}
 }
