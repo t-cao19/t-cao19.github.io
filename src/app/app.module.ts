b0e5c2e9cc1d6c505d939c34f9fa749906279671
Tony Cao <tony.cao1@outlook.com>
2020-10-15 22:14:09 -0400

    Created side bar for resume page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SidebarBoxComponent } from './components/sidebar-box/sidebar-box.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ResumeComponent,
     VolunteerComponent,
     CarouselComponent,
+    SidebarBoxComponent,
   ],
   imports: [
     BrowserModule,
