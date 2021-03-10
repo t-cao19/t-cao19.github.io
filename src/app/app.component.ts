6b5331debcbfb1f1ccdab29b912a9e94844c65ff
Tony Cao <tony.cao1@outlook.com>
2021-03-09 19:04:29 -0500

    Added google analytics code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {filter} from 'rxjs/operators';
+declare var gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit, OnDestroy {
   title = 'Tony Cao';
+  private routerSubscription: Subscription;
+  constructor(private router: Router) {
+  }
+
+  ngAfterViewInit(): void {
+    // subscribe to router events and send page views to Google Analytics
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        gtag('config', 'G-ZWT2NCJEPH', {'page_path': event.urlAfterRedirects});
+      });
+  }
+  ngOnDestroy(): void {
+    this.routerSubscription.unsubscribe();
+  }
 }
