ee1bec169312cec071e9b4fe5fe806d44cfb3b1e
Tony Cao <tony.cao1@outlook.com>
2021-08-19 12:47:38 -0400

    Updated footer icon colour + removed unneeded json

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
   faEnvelope = faEnvelope;
   faLinkedinIn = faLinkedinIn;
   faGitHub = faGithub;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import works from '../../../assets/data/work.json';
 import experiences from '../../../assets/data/experience.json';
 
 @Component({
@@ -10,7 +9,6 @@ import experiences from '../../../assets/data/experience.json';
   styleUrls: ['./resume.component.scss'],
 })
 export class ResumeComponent implements OnInit {
-  works: any[];
   experiences: any[];
   faEnvelope = faEnvelope;
   faLinkedinIn = faLinkedinIn;
@@ -18,7 +16,6 @@ export class ResumeComponent implements OnInit {
   education: any;
 
   constructor() {
-    this.works = works;
     this.experiences = experiences;
     this.education = {
       employer: '4th Year Student',
