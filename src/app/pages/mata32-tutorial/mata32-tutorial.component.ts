10a9ded0974039827b008fb3fa2dd5caf86babfe
Tony Cao <tony.cao1@outlook.com>
2021-09-17 18:33:47 -0400

    Added tutorial 1 notes for MATA32

--- src/app/pages/mata32-tutorial/mata32-tutorial.component.ts
+++ src/app/pages/mata32-tutorial/mata32-tutorial.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import tutorials from '../../../assets/data/mata32f21-tutorial.json';
 
 @Component({
   selector: 'app-mata32-tutorial',
@@ -14,7 +15,7 @@ export class MATA32TutorialComponent implements OnInit {
   faGitHub = faGithub;
 
   constructor() {
-    this.tutorials = [];
+    this.tutorials = tutorials;
   }
 
   ngOnInit(): void {}
