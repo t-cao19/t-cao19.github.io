eb4c7f52c8036ba524165316eca4c754a42fbf49
Tony Cao <tony.cao1@outlook.com>
2022-01-05 21:10:58 -0500

    Final updates

--- src/app/pages/teaching/teaching.component.ts
+++ src/app/pages/teaching/teaching.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import teachings from '../../../assets/data/teaching.json';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-teaching',
@@ -19,5 +20,12 @@ export class TeachingComponent implements OnInit {
     this.teachings = teachings;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    AOS.init({
+      delay: 200,
+      duration: 1500,
+      once: false,
+      anchorPlacement: 'top-bottom',
+    });
+  }
 }
