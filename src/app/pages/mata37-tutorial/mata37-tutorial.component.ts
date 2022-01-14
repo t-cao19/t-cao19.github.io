5aeb70378a667e4cb674e99879e970e5f65acd0b
Tony Cao <tony.cao1@outlook.com>
2022-01-14 17:40:13 -0500

    Fixed unreadable tutorial links

--- src/app/pages/mata37-tutorial/mata37-tutorial.component.ts
+++ src/app/pages/mata37-tutorial/mata37-tutorial.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { TimelineItem } from 'src/app/components/ngx-vertical-timeline/timeline-item';
-import tutorials from '../../../assets/data/mata37-tutorial.json';
+import tutorialsW21 from '../../../assets/data/mata37w21-tutorial.json';
 
 @Component({
   selector: 'app-mata37-tutorial',
@@ -13,36 +13,21 @@ export class MATA37TutorialComponent implements OnInit {
   faEnvelope = faEnvelope;
   faLinkedinIn = faLinkedinIn;
   faGitHub = faGithub;
-  winter21Notes: any[];
-  winter22Notes: any[];
 
   // For vertical timeline
   winter21Timeline: TimelineItem[] = [];
   winter22Timeline: TimelineItem[] = [];
 
   constructor() {
-    this.winter21Notes = [];
-    this.winter22Notes = [];
-
-    for (let i = 0; i < tutorials.length; i++) {
-      let note = tutorials[i];
-      console.log(note);
-      if (note.term == 'W21') {
-        this.winter21Notes.push(note);
-      } else {
-        this.winter22Notes.push(note);
-      }
-    }
-
-    for (let i = 0; i < this.winter21Notes.length; i++) {
+    for (let i = 0; i < tutorialsW21.length; i++) {
       this.winter21Timeline.push({
-        title: 'Week ' + (this.winter21Notes.length - i + 1),
-        date: this.winter21Notes[i].date,
-        bullets: this.winter21Notes[i].topics,
+        title: 'Week ' + (tutorialsW21.length - i + 1),
+        date: tutorialsW21[i].date,
+        bullets: tutorialsW21[i].topics,
         label: 'Notes',
         command() {
-          if (this.winter21Notes[i].notes) {
-            window.open(this.winter21Notes[i].notes, '_blank').focus();
+          if (tutorialsW21[i].notes) {
+            window.open(tutorialsW21[i].notes, '_blank').focus();
           } else {
             window.open('/home').focus();
           }
