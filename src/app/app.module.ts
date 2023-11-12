5a33d52452478c2197089b37e2973bee730e09f0
Tony Cao <tony.cao1@outlook.com>
2023-11-12 13:46:55 -0500

    Code clean up of irrelevant code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,14 @@ import { ProjectsComponent } from './pages/projects/projects.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ResumeComponent } from './pages/resume/resume.component';
-import { VolunteerComponent } from './pages/volunteer/volunteer.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterComponent } from './components/footer/footer.component';
-import { TutorialBoxComponent } from './components/tutorial-box/tutorial-box.component';
 import { ProjectBoxComponent } from './components/project-box/project-box.component';
 import { ExperienceBoxComponent } from './components/experience-box/experience-box.component';
-import { InfiniteTypeDeleteModule } from 'ngx-sbz-type-delete';
 import { MatTabsModule } from '@angular/material/tabs';
 import { FeaturedWorkComponent } from './components/featured-work/featured-work.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -31,9 +28,7 @@ import { ResumeExperienceComponent } from './components/resume-experience/resume
     ProjectsComponent,
     NavbarComponent,
     ResumeComponent,
-    VolunteerComponent,
     FooterComponent,
-    TutorialBoxComponent,
     ProjectBoxComponent,
     ExperienceBoxComponent,
     FeaturedWorkComponent,
@@ -49,7 +44,6 @@ import { ResumeExperienceComponent } from './components/resume-experience/resume
     MatFormFieldModule,
     MatInputModule,
     FontAwesomeModule,
-    InfiniteTypeDeleteModule,
     MatTabsModule,
     FormsModule,
     ReactiveFormsModule,
